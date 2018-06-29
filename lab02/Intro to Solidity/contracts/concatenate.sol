44d1290bf0bcbae21ab03e88f4dabd4cc7bcd173
Adinda Savitri <savitri.adinda@gmail.com>
2018-06-29 15:16:12 +0800

    concatenation contract

--- lab02/Intro to Solidity/contracts/concatenate.sol
+++ lab02/Intro to Solidity/contracts/concatenate.sol
@@ -2,10 +2,25 @@ pragma solidity 0.4.19;
 
 
 contract Concatenate {
+    // transaction cost 26383 gas
+    // execution cost 3447 gas
     function concatWithoutImport(string _s, string _t) public returns (string) {
-    }
+        bytes memory sBytes = bytes(_s);
+        bytes memory tBytes = bytes(_t);
 
-    /* BONUS */
-    function concatWithImport(string s, string t) public returns (string) {
+        uint length1 = sBytes.length;
+        uint length2 = tBytes.length;
+        uint lengthTotal = length1 + length2;
+
+        bytes memory result = new bytes(lengthTotal);
+        for (uint i = 0; i < length1; i++) {
+            result[i] = sBytes[i];
+        }
+
+        for (uint j = 0; j < length2; j++) {
+            result[j+length1] = tBytes[j];
+        }
+
+        return string(result);
     }
 }
