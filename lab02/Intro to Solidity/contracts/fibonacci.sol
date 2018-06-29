9f357abbe1784a050c51731effa0755e1441cb68
Adinda Savitri <savitri.adinda@gmail.com>
2018-06-29 11:10:16 +0800

    fibonacci contract

--- lab02/Intro to Solidity/contracts/fibonacci.sol
+++ lab02/Intro to Solidity/contracts/fibonacci.sol
@@ -4,9 +4,23 @@ pragma solidity 0.4.19;
 contract Fibonacci {
     /* Carry out calculations to find the nth Fibonacci number */
     function fibRecur(uint n) public pure returns (uint) {
+        if (n < 2) {
+            return n;
+        }
+
+        return fibRecur(n - 1) + fibRecur(n - 2);
     }
 
     /* Carry out calculations to find the nth Fibonacci number */
-    function fibIter(uint n) public returns (uint) {
+    function fibIter(uint n) public pure returns (uint) {
+        uint lo = 0;
+        uint hi = 1;
+
+        for (uint i = 0; i < n; i++) {
+          hi = lo + hi;
+          lo = hi - lo;
+        }
+
+        return lo;
     }
 }
