f7eb07c5c3f6393648ac8b03032c84ac548edd85
Adinda Savitri <savitri.adinda@gmail.com>
2018-06-29 11:09:57 +0800

    greeter contract

--- lab02/Intro to Solidity/contracts/greeter.sol
+++ lab02/Intro to Solidity/contracts/greeter.sol
@@ -4,9 +4,11 @@ pragma solidity 0.4.19;
 contract Greeter {
     string private greeting;
 
-    function Greeter(string _greeter) public {
+    function Greeter(string _greeting) public {
+      greeting = _greeting;
     }
 
     function greet() public view returns (string) {
+      return greeting;
     }
 }
