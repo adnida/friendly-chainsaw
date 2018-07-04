86525279187ef9a5f4472993a0c780b9ccda737e
Adinda Savitri <savitri.adinda@gmail.com>
2018-07-04 17:23:34 +0800

    add boolean flag to customer entity

--- food-ordering/contracts/Customers.sol
+++ food-ordering/contracts/Customers.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.4.18;
 contract Customers {
     struct Customer {
         string deliveryAddress;
+        bool isCustomer;
     }
 
     mapping (address => Customer) public customers;
@@ -11,6 +12,11 @@ contract Customers {
 
     function registerAsCustomer(string deliveryAddress) public {
         customers[msg.sender].deliveryAddress = deliveryAddress;
+        customers[msg.sender].isCustomer = true;
         emit CustomerRegisteredEvent(msg.sender, deliveryAddress);
+    }
+
+    function checkIsCustomer(address _address) public view returns (bool) {
+        return customers[_address].isCustomer;
     }
 }
