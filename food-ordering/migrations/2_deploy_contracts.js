20a84f438ddc72ad158a9c901b07f7887d4b3667
Adinda Savitri <savitri.adinda@gmail.com>
2018-07-10 00:09:16 +0800

    deploy contracts with addresses as constructor params

--- food-ordering/migrations/2_deploy_contracts.js
+++ food-ordering/migrations/2_deploy_contracts.js
@@ -4,8 +4,15 @@ const Customers = artifacts.require("./Customers.sol");
 const Orders = artifacts.require("./Orders.sol");
 
 module.exports = function(deployer) {
-	deployer.deploy(FoodDelivery);
-	deployer.deploy(Restaurants);
-	deployer.deploy(Customers);
-	deployer.deploy(Orders);
+	deployer.deploy(Restaurants)
+	.then(() => {
+		return deployer.deploy(Customers);
+	})
+	.then(() => {
+		console.log("Customers contract deployed at", Customers.address);
+		return deployer.deploy(Orders, Restaurants.address, Customers.address);
+	})
+	.then(() => {
+		return deployer.deploy(FoodDelivery, Restaurants.address, Customers.address, Orders.address);
+	});
 };
