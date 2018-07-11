c0a9b6653058d2023bc15e3a838758a99c74bc54
Adinda Savitri <savitri.adinda@gmail.com>
2018-07-11 09:02:31 +0800

    use deployed instead of new for instance hook, add validation test

--- food-ordering/test/customers-test.js
+++ food-ordering/test/customers-test.js
@@ -5,12 +5,17 @@ contract("Customers", accounts => {
     let instance;
 
     it("can register as a customer", async() => {
-      instance = await Customers.new();
+      instance = await Customers.deployed();
 
       let expectedDeliveryAddress = "5 Stevens Drive #01-01";
       await instance.registerAsCustomer(expectedDeliveryAddress, { from: account1 });
       let [deliveryAddress, isCustomer] = await instance.customers(account1);
 
       assert.equal(deliveryAddress, expectedDeliveryAddress);
+    });
+
+    it("can validate a customer", async() => {
+      assert(await instance.checkIsCustomer(account1));
+      assert.equal(await instance.checkIsCustomer(account2), false);
     });
 });
